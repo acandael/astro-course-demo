2802317901362bf83ef4045ead4de407c1266ae0
Anthony Candaele <anthonycandaele@Anthonys-Mac-Studio.local>
2024-01-07 11:36:23 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind(), svelte()],
+  site: "https://rhythm.nation",
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 });
